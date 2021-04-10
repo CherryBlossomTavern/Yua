ff823248520b22f0dfcd0bf9a8b26483e36d5336
Nobu <nowobuwu@gmail.com>
2021-04-09 21:21:10 -0500

    yua v1.5.2

--- src/yua.ts
+++ src/yua.ts
@@ -8,5 +8,5 @@ export default (yua: import('./client')): void => {
   yua.client.on('messageCreate', messageCreate(yua))
   
   // I write new code done for night gonna push
-
+  // Last Bit of code, yua v1.5.2
 }
